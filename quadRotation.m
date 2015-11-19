55462b13f9adf93eb0ac72d1c5f6882bbde93d20
Andrew Wade <fincle@users.noreply.github.com>
2015-11-19 13:50:41 +1100

    Added twice the phase evolution to the pump field
    Not sure if these is correct.  Intuitively it seems that the pump phase evolves at twice the rate of the fundamental.  Need to make absolutely clear to the user that this is the assumption so that they don't try to preempt it in the calling function.

--- quadRotation.m
+++ quadRotation.m
@@ -3,5 +3,5 @@ function M_rotate = quadRotation(phi)
 %Computes a 4x4 matrix for rotating quadratures of A and B, to be
 %multiplied by Theta functions, for V, need to square everything.
 
-M_rotate = [cos(phi) sin(phi) 0 0; -sin(phi) cos(phi) 0 0; ...
-    0 0 cos(phi) sin(phi); 0 0 -sin(phi) cos(phi)];
+M_rotate = [cos(phi) sin(phi) 0 0;-sin(phi) cos(phi) 0 0;...
+    0 0 cos(2.*phi) sin(2.*phi);0 0 -sin(2.*phi) cos(2.*phi)];
